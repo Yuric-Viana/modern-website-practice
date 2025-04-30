389507e3e3221559ad962f217054b6ab263bf3d0
Yuri Viana <ycviana@sga.pucminas.br>
2025-04-30 07:57:15 -0300

    Inicio de projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+const navText = document.querySelector('.nav-menu__list');
+const navTextList = document.querySelector('.nav-menu__rec')
+const listaMotion = document.querySelector('.submenu--motion');
+const listaRec = document.querySelector('.submenu--resources')
+
+navText.addEventListener('mouseover', () => {
+  if(listaMotion.style.display === 'none') {
+    listaMotion.style.display = 'block';
+  } else {
+    listaMotion.style.display = 'none';
+  }
+  navText.addEventListener('mouseout', () => {
+    listaMotion.style.display = 'none';
+  })
+})
+
+navTextList.addEventListener('mouseover', () => {
+  if(listaRec.style.display === 'none') {
+    listaRec.style.display = 'block';
+  } else {
+    listaRec.style.display = 'none';
+  }
+  navTextList.addEventListener('mouseout', () => {
+    listaRec.style.display = 'none';
+  })
+})
